dbeb5bc6cd7b696f8cc110f3f43937bb9c8ac2b2
Debasish Sahoo <100151159+DebasishSahoo10@users.noreply.github.com>
2022-06-09 22:10:49 +0530

    fix : added negative value validator

--- triangle-area.js
+++ triangle-area.js
@@ -5,8 +5,12 @@ var output = document.querySelector('#output-area')
 
 
 function calculateArea() {
-    var area = ((base.value*height.value) / 2);
-    output.innerText = "And the area is : " + area;
+    if (base.value >= 1 && height.value >=1) {
+        var area = ((base.value*height.value) / 2);
+        output.innerText = "And the area is : " + area;
+    } else {
+        output.innerText = "Please Input positive values"
+    }
 }
 
 
